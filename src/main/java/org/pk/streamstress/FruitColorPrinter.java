804c173a3823ecd0ea5d6c6ecb62b69d85787778
pkamath2 <git.pkamath2@gmail.com>
2018-05-21 16:48:51 +0800

    More alt methods

--- src/main/java/org/pk/streamstress/FruitColorPrinter.java
+++ src/main/java/org/pk/streamstress/FruitColorPrinter.java
@@ -4,11 +4,11 @@ public class FruitColorPrinter {
 
     // Color code from https://gist.github.com/chrisopedia/8754917
     // Beer mug for bash: http://www.fileformat.info/info/unicode/char/1f37a/index.htm
-    private static final String RESET = "\u001b[0m";
-    private static final String RED = "\u001b[1;31m";
-    private static final String GREEN = "\u001b[1;32m";
-    private static final String YELLOW = "\u001b[1;33m";
-    private static final String ORANGE = "\u001b[1;35m";
+    private static String RESET = "\u001b[0m";
+    private static String RED = "\u001b[1;31m";
+    private static String GREEN = "\u001b[1;32m";
+    private static String YELLOW = "\u001b[1;33m";
+    private static String ORANGE = "\u001b[1;35m";
 
     public static void printWithColor(Fruit fruit){
 
@@ -23,7 +23,7 @@ public class FruitColorPrinter {
                 System.out.print(GREEN);
                 break;
             case "Orange":
-                System.out.print(ORANGE);
+                System.out.print(ORANGE);//No oranges for you!
                 break;
         }
         System.out.println(fruit);
